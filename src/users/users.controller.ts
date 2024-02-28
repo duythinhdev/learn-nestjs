00b1a959d4fcc82859ff6569ef0f1c01ea0efbd3
duythinhdev <do.duythinh1@gmail.com>
2024-02-29 06:40:04 +0700

    practice 3 tranform validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get,Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserDto } from '../user.dto';
 
 @Controller('users')
 export class UserController {
     constructor() {}
 
-    @Get()
-    getAllUsers(): Array<any> {
-        return [{name: 'giang', age: 18}, {name: 'binh', age: 18}];
-    }
     @Post()
-    createUser() {
-        return {name: 'giang', age: 18};
+    createUser(@Body() user: UserDto): UserDto {
+        return { username: 'test', password: 'test'};
+    }
+    @Get(':id')
+    getUserById(@Param('id') id: number){
+        console.log(id)
+        return 'test';
     }
 
 }
